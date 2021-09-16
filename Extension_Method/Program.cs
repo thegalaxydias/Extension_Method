a0263bbd50ef5b7cd4272a2bea0ca37f76c91a0e
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-16 19:33:11 +0300

    Добавьте файлы проекта.

---
+++ Extension_Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Extension_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            value.ExtensionMethod();
+        }
+
+
+    }
+    public static class Extension
+    {
+        public static void ExtensionMethod(this int value)
+        {
+            Console.WriteLine("Value of your variable is: " + value);
+        }
+    }
+
+
+     
+}
+
